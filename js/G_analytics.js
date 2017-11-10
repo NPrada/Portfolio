9ec5ad4ec61bbae6a7c80ad52a2195c26b4698c4
Nimajik <niccolo.prada@gmail.com>
2017-11-10 16:39:11 +0100

    Cleaned up headers

---
+++ js/G_analytics.js
@@ -0,0 +1,6 @@
+//this is used to track traffic in google analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-109450808-1');
